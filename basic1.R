1aea7eadae36208a240cde8d487e8d17565719c4
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-03-07 04:07:34 +0530

    Create basic1.R

---
+++ basic1.R
@@ -0,0 +1,30 @@
+#Addition of two number
+x <- 10
+y <- 25
+z <- sum(x,y)
+print(z)
+
+#output - 35
+
+#define a vector
+a<-c(1,2,3,4)
+print(a)
+
+#Output : [1] 1 2 3 4
+
+#Addition,subtraction,multiplication,division from `a` vector
+a+5
+a-10
+a*8
+a/7
+
+#output : 6 7 8 9
+#[1] -9 -8 -7 -6
+#[1]  8 16 24 32
+#[1] 0.1428571 0.2857143 0.4285714 0.5714286
+
+#Saving result another vector
+b <- a - 10
+b
+
+#Output : -9 -8 -7 -6
