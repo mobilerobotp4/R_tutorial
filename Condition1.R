d624e9280abb008672a388231d8fb2ae19c72401
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2019-07-29 14:00:37 -0400

    Create Condition1.R

---
+++ Condition1.R
@@ -0,0 +1,31 @@
+# Conditional Statement - The if Statement
+#if (TRUE) print ("Message")
+#Example
+
+x <- 5
+if(x > 0){
+print("Positive number")
+}
+
+#Output : "Positive number"
+
+# if - else statement
+# typing else on the same line where closing  if }
+
+
+x <- -5
+if(x > 0){
+print("Positive number")
+}else {
+    print("Negative number")
+}
+
+#Output : "Negative number"
+
+# The above conditional can also be written in a single line as follows:
+
+x <- -5
+if(x > 0) print("Non-negative number") else print("Negative number")
+
+#Output: "Negative number"
+
